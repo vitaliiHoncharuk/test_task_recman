9263bebed3287508de28ce2e2af74f8e55ed131c
vitalii.honcharuk <vitaliihoncharuk@gmail.com>
2025-07-12 14:16:50 +0300

    fix build

--- src/features/board/types/enums.ts
+++ src/features/board/types/enums.ts
@@ -1,5 +1,7 @@
-export enum CompletionFilter {
-  ALL = 'all',
-  COMPLETED = 'completed',
-  UNCOMPLETED = 'uncompleted'
-}
+export const CompletionFilter = {
+  ALL: 'all',
+  COMPLETED: 'completed',
+  UNCOMPLETED: 'uncompleted'
+} as const;
+
+export type CompletionFilter = typeof CompletionFilter[keyof typeof CompletionFilter];
